f7323f02bd4bbe603ef9b4e0bc3f2e6e35688197
Anderson Ramos <anderson.ramos@zoom.com.br>
2020-04-11 15:57:13 -0300

    CRUD and tests

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require('uuidv4');
 
 // const { uuid } = require("uuidv4");
 
@@ -10,24 +11,83 @@ app.use(cors());
 
 const repositories = [];
 
+//list
 app.get("/repositories", (request, response) => {
-  // TODO
+  
+  return response.json(repositories);
 });
 
+//create
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = { 
+    id: uuid(), 
+    title, 
+    url, 
+    techs, 
+    likes: 0
+  };
+  
+  repositories.push(repository);
+
+  return response.status(200).json(repository)
 });
 
+// update
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs , likes} = request.body;
+
+  const repoIndex = repositories.findIndex( repo => repo.id === id);
+
+  if(repoIndex <  0){
+    return response.status(400).json({error: "repository does not exist"});
+  }
+
+  const repoTemp = {
+    id,
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories[repoIndex] = repoTemp;
+
+  return response.status(200).json(repoTemp);
+
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex( repo => repo.id === id);
+
+  if(repoIndex <  0){
+    return response.status(400).send();
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).send();
 });
 
+//update like
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex( repo => repo.id === id);
+
+  if(repoIndex <  0){
+    return response.status(400).send();
+  }
+
+  const repository = repositories.find(repository => repository.id === id)
+  repository.likes++;
+  
+  return response.status(200).json(repository);
+
 });
 
 module.exports = app;
